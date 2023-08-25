dab94223db7fee4eddb20a26cc3357ac873d1bb0
developmentHF <development.henry@gmail.com>
2023-08-25 18:30:11 +1000

    basic ticket model

--- backend/TicketPal/admin.py
+++ backend/TicketPal/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Ticket
+
+
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'date', 'vendor', 'phone')
+
 
 # Register your models here.
+admin.site.register(Ticket, TicketAdmin)
+
--- backend/TicketPal/models.py
+++ backend/TicketPal/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Ticket(models.Model):
+    name = models.CharField(max_length=120)
+    location = models.CharField(max_length=120)
+    date = models.DateTimeField()
+    vendor = models.ForeignKey(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=12)
+
+    def _str_(self):
+        return self.name
