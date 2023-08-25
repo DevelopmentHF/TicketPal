cdbf7be4ee2abb2504b004c3f755043abe957508
Kaiyuan Qian <116858989+kaiyuanqian@users.noreply.github.com>
2023-08-25 18:59:37 +1000

    get frontend up and running

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const tempTickets = [
+  {
+    name: "BTV",
+    location: "Barunah Plains",
+    date: "2023-12-28",
+    vendor: "Kaiyuan Qian",
+  },
+  {
+    name: "Taylor Swift",
+    location: "MCG",
+    date: "2024-02-16",
+    vendor: "Henry Feelding",
+  },
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewCompleted: false,
+      ticketList: tempTickets,
+    }
+  }
+
+  displayCompleted = (status) => {
+    if (status) {
+      return this.setState({ viewCompleted: true });
+    }
+
+    return this.setState({ viewCompleted: false });
+  };
+
+  renderItems = () => {
+    const { viewCompleted } = this.state;
+    const newTickets = this.state.ticketList;
+
+    
+    return newTickets.map((item) => (
+      <div className="ticket">
+        {item.name}
+      </div>
+    
+
+    ));
+  };
+  
+  render() {
+    return (
+      <div className="container">
+        Test {this.renderItems()}
+      </div>
+      
+    );
+  }
 }
 
 export default App;
