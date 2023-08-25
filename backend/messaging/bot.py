dd578b19d34cd5c1c95e70e997f2cb0946edaec1
esugunananth <enrique.sugunananthan@student.unimelb.edu.au>
2023-08-25 23:18:04 +1000

    add code for backend ticket interaction

--- backend/messaging/bot.py
+++ backend/messaging/bot.py
@@ -1,14 +1,23 @@
 from twilio.rest import Client
+from env import API_KEY
+import requests
 
 account_sid = 'AC33ff820d3d035cfd698e73eb155e9044'
-auth_token = '0cb4ec3c29e2a1e4993d1f063eea2f0a'
+auth_token = API_KEY
 client = Client(account_sid, auth_token)
 
-message = client.messages \
-                .create(
-                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-                     from_='+15189812165',
-                     to='+61472909030'
-                 )
+#pull data from third party rest api
+response = requests.get('http://127.0.0.1:8000/api/tickets')
 
-print(message.sid)
+#convert response data into json
+users = response.json()
+print(users[0])
+    
+#message = client.messages \
+                #.create(
+                     #body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                     #from_='+15189812165',
+                     #to='+61472909030'
+                 #)
+
+#print(message.sid)
