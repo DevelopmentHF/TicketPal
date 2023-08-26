39f6f6454ffca9a158e88bb3d06778d360fcac67
developmentHF <development.henry@gmail.com>
2023-08-26 16:40:51 +1000

    make max bidder not null optional

--- backend/TicketPal/models.py
+++ backend/TicketPal/models.py
@@ -9,7 +9,7 @@ class Ticket(models.Model):
     date = models.DateTimeField()
     expiry = models.DateTimeField(auto_now_add=True)
     bidders = models.ManyToManyField(User, related_name='tickets_as_bidders', blank=True)  # Specify a related_name
-    maxBidder = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='tickets_as_max_bidder')  # Specify a related_name
+    maxBidder = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='tickets_as_max_bidder', blank=True)  # Specify a related_name
     vendor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="tickets_as_vendor")
     phone = models.CharField(max_length=12)
     curMaxBid = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(1000)], default=0)
