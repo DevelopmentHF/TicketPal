385dac798f42850fee6ba898ece23c5176cfa9b6
developmentHF <development.henry@gmail.com>
2023-08-26 17:22:46 +1000

    style tweaks

--- frontend/src/components/Navigations.js
+++ frontend/src/components/Navigations.js
@@ -79,7 +79,7 @@ export function Navigation() {
 
     return (
     <div>
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-200 p-4">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost btn-circle">
