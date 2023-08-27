fa41cf548bd78b9da2954615d659c0cfe1c2564a
enriquesugu <130334791+enriquesugu@users.noreply.github.com>
2023-08-27 14:48:31 +1000

    modify sms message

--- backend/messaging/bot.py
+++ backend/messaging/bot.py
@@ -38,7 +38,7 @@ while True:
                     message = client.messages \
                         .create(
                         body="Congragulations " + user.get("username") + "! Your " + ticket["name"] + 
-                        " ticket was bought for $" + str(ticket["curMaxBid"]) +" by null",
+                        " ticket was bought for $" + str(ticket["curMaxBid"]) + ".",
                         from_='+15189812165',
                         to= '+61472909030'
                     )
