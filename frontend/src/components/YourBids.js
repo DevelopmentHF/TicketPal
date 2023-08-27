8a384aa1e5472ac4bbd302830787ce277931c07e
developmentHF <development.henry@gmail.com>
2023-08-27 14:33:40 +1000

    proper state for bottom half things

--- frontend/src/components/YourBids.js
+++ frontend/src/components/YourBids.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-// makes the carousel for featured tickets
 class YourBids extends Component {
     constructor(props) {
         super(props);
@@ -9,54 +8,45 @@ class YourBids extends Component {
             ticketList: [],
             yourBids: [],
             userId: props.id,
-            // isTicketShowing: false,
-        }
-    };
-
-    /* this is grotty because its running every second */
-    componentDidMount() {
-        this.refreshList();
-        // only way ive figured out how to refresh the list
-        this.intervalId = setInterval(this.refreshList, 1000);
-    };
-
-    componentWillUnmount() {
-        clearInterval(this.intervalId); // Clear interval when component unmounts
+        };
     }
 
-    // get tickets from backend
-    // TODO: MAKE THIS FUNCTION RUN WHEN WE CREATE A NEW TICKET
+    componentDidMount() {
+        this.refreshList();
+        this.intervalId = setInterval(this.refreshList, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.intervalId);
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.id !== this.props.id) {
+            this.setState({ userId: this.props.id }, () => {
+                this.refreshList();
+            });
+        }
+    }
+
     refreshList = () => {
-        console.log("refresh");
         axios
             .get("http://localhost:8000/api/tickets/")
-            .then((res) => this.setState({ticketList: res.data}))
+            .then((res) => {
+                this.setState({ ticketList: res.data });
+                this.updateYourBids();
+            })
             .catch((err) => console.log(err));
+    }
 
-        // go through the ticket list and remove all the ones not related to this persons id
-        this.state.yourBids = [];
-        this.state.ticketList.map((item) => {
-            if (item.bidders.includes(this.state.userId)) {
-                console.log("CurID: " + this.state.userId);
-                console.log("bid match");
-                this.state.yourBids.push(item);
-            } else {
-                console.log("no bid match");
-                console.log("CurID: " + this.state.userId);
-            }
-        })
+    updateYourBids = () => {
+        const { userId, ticketList } = this.state;
+        const yourBids = ticketList.filter((item) => item.bidders.includes(userId));
+        this.setState({ yourBids });
     }
 
     renderItems = () => {
-
-        // Call the function every 1000 milliseconds (1 second)
-        // const intervalId = setInterval(this.refreshList, 2000);
-
-        const { viewCompleted } = this.state;
-        const newTickets = this.state.ticketList;
-
         return this.state.yourBids.map((item) => (
-            <h1>{item.name}</h1>
+            <h1 key={item.id}>{item.name}</h1>
         ));
     };
 
--- frontend/src/components/YourTickets.js
+++ frontend/src/components/YourTickets.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-// makes the carousel for featured tickets
 class YourTickets extends Component {
     constructor(props) {
         super(props);
@@ -9,54 +8,46 @@ class YourTickets extends Component {
             ticketList: [],
             yourTickets: [],
             userId: props.id,
-            // isTicketShowing: false,
-        }
-    };
-
-    /* this is grotty because its running every second */
-    componentDidMount() {
-        this.refreshList();
-        // only way ive figured out how to refresh the list
-        this.intervalId = setInterval(this.refreshList, 1000);
-    };
-
-    componentWillUnmount() {
-        clearInterval(this.intervalId); // Clear interval when component unmounts
+        };
     }
 
-    // get tickets from backend
-    // TODO: MAKE THIS FUNCTION RUN WHEN WE CREATE A NEW TICKET
+    componentDidMount() {
+        this.refreshList();
+        this.intervalId = setInterval(this.refreshList, 1000);
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.id !== this.props.id) {
+            this.setState({ userId: this.props.id }, () => {
+                // Once the userId state is updated, refresh the list
+                this.refreshList();
+            });
+        }
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.intervalId);
+    }
+
     refreshList = () => {
-        console.log("refresh");
         axios
             .get("http://localhost:8000/api/tickets/")
-            .then((res) => this.setState({ticketList: res.data}))
+            .then((res) => {
+                this.setState({ ticketList: res.data });
+                this.updateYourTickets();
+            })
             .catch((err) => console.log(err));
+    }
 
-        // go through the ticket list and remove all the ones not related to this persons id
-        this.state.yourTickets = [];
-        this.state.ticketList.map((item) => {
-            if (item.vendor === this.state.userId) {
-                console.log("CurID: " + this.state.userId);
-                console.log("ticket match");
-                this.state.yourTickets.push(item);
-            } else {
-                console.log("no match");
-                console.log("CurID: " + this.state.userId);
-            }
-        })
+    updateYourTickets = () => {
+        const { userId, ticketList } = this.state;
+        const yourTickets = ticketList.filter((item) => item.vendor === userId);
+        this.setState({ yourTickets });
     }
 
     renderItems = () => {
-
-        // Call the function every 1000 milliseconds (1 second)
-        // const intervalId = setInterval(this.refreshList, 2000);
-
-        const { viewCompleted } = this.state;
-        const newTickets = this.state.ticketList;
-
         return this.state.yourTickets.map((item) => (
-            <h1>{item.name}</h1>
+            <h1 key={item.id}>{item.name}</h1>
         ));
     };
 
