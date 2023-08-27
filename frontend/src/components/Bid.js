971bc448aae43fc504fe3c3a94d7550c2e153f48
developmentHF <development.henry@gmail.com>
2023-08-27 14:17:34 +1000

    broken fuck

--- frontend/src/components/Bid.js
+++ frontend/src/components/Bid.js
@@ -29,22 +29,25 @@ class Bid extends Component {
         this.setState({ isPriceInputOpen: false});
     };
 
-    enterBid = (event) => {
-        const specificTicket = this.state.ticketList.find(obj => obj.id === this.curId);
-        console.log(this.state.ticketList);
+    enterBid = async (event) => {
+        event.preventDefault(); // Prevent the default form submission behavior
+        this.refreshList();
+        const specificTicket = this.state.ticketList.find(obj => obj.id === this.state.curId); // Use this.state.curId
         const formData = new FormData(event.target);
         const bidPrice = parseInt(formData.get("curMaxBid"));
+
         if (bidPrice > specificTicket.curMaxBid) {
             console.log("BID!");
+
             try {
-                const response = fetch(`http://localhost:8000/api/tickets/${this.state.curId}`, {
+                const response = await fetch(`http://localhost:8000/api/tickets/${this.state.curId}`, {
                     method: 'PUT', // or 'PATCH' depending on your API's requirements
                     body: formData,
                     headers: {
                         // 'Authorization': `Bearer ${localStorage.getItem('access_token')}`
                     }
                 });
-        
+
                 if (response.ok) {
                     // Handle success, maybe close the modal or show a success message
                     console.log("Ticket updated");
@@ -57,7 +60,40 @@ class Bid extends Component {
                 console.error('Error updating ticket:', error);
             }
         } else {
-            console.log("fuck");
+            console.log("Bid price must be higher");
+        }
+    }
+
+    testfunc =  (event) => {
+        event.preventDefault(); // Prevent the default form submission behavior
+        this.refreshList();
+        console.log("TICKETS TICKETS TICKETS: ", this.state.ticketList);
+        const tickets = this.state.ticketList;
+        for (let i=0; i < tickets.length; i++) {
+            console.log("HEFHUHEF: ", tickets[i]);
+            console.log("TICKETID: ", tickets[i].id);
+            if (this.state.curId === tickets[i].id) {
+                try {
+                    axios.put(`/api/tickets/${this.state.curId}`, {
+                        curMaxBid: 1000,
+                    });
+
+                    // // Assuming your API returns an updated ticket object, update the state with the new ticket
+                    // const updatedTicket = response.data; // Adjust based on your API response format
+                    // const ticketList = this.state.ticketList.map((ticket) =>
+                    //     ticket.id === updatedTicket.id ? updatedTicket : ticket
+                    // );
+                    //
+                    // this.setState({
+                    //     ticketList,
+                    //     curMaxBidInput: "", // Clear the input field after update
+                    //     isPriceInputOpen: false, // Close the input form
+                    // });
+                } catch (error) {
+                    // Handle error
+                    console.error("Error updating ticket:", error);
+                }
+            }
         }
     }
 
@@ -67,11 +103,11 @@ class Bid extends Component {
                 <button className="btn" onClick={this.openPriceInput}>Bid</button>
                 { this.state.isPriceInputOpen && (
                     <div>
-                        <form onSubmit={this.enterBid}>
+                        <form onSubmit={this.testfunc}>
                             <input type="text" name="curMaxBid" placeholder="Type here" className="input input-bordered input-info w-full max-w-xs m-2" />
                             <button className="btn" type="submit">Enter</button>
                         </form>
-                        
+
                     </div>
                 )}
             </div>
